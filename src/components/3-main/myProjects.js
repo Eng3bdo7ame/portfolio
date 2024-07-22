6fb9b2d347e5509f53e2222ce1ea78efaeaca28a
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2024-07-22 20:19:01 +0300

    first commit

---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,53 @@
+export const myProjects = [
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "./images/1.jpg",
+  },
+
+  {
+    projectTitle: "react & css project",
+    category: ["react", "css"],
+    imgPath: "./images/2.jpg",
+  },
+
+  {
+    projectTitle: "css project",
+    category: ["css"],
+    imgPath: "./images/3.jpg",
+    //https://eng3bdo7ame.github.io/shopping_shose/
+  },
+  {
+    projectTitle: "react project",
+    category: ["react"],
+    imgPath: "./images/4.jpg",
+  },
+  {
+    projectTitle: "css project",
+    category: ["css"],
+    imgPath: "./images/5.jpg",
+    //https://eng3bdo7ame.github.io/car_web/
+  },
+
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/6.jpg",
+  },
+  {
+    projectTitle: "node project",
+    category: ["HTML", "css", "js"],
+    imgPath: "./images/7.jpg",
+    //https://eng3bdo7ame.github.io/House/
+  },
+  {
+    projectTitle: "node project",
+    category: ["node"],
+    imgPath: "./images/8.jpg",
+  },
+  {
+    projectTitle: "javaScript project",
+    category: ["js"],
+    imgPath: "./images/9.jpg",
+  },
+];
